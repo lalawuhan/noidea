fa4fa7c7ad36ba68fca6c74756e14128aacb288d
Rethabile Segoe <precious.segoe@gmail.com>
2020-07-29 16:15:11 +0200

    remove older code

--- src/components/useArticles.js
+++ src/components/useArticles.js
@@ -1,14 +1,6 @@
 import { useState, useEffect, useReducer } from "react";
 export default function useArticles(initialUrl, initialData) {
   const [url, setUrl] = useState(initialUrl);
-  //   const [data, setData] = useState(initialData);
-
-  //   const [data, setData] = useState({ hits: [] });
-  //   const [url, setUrl] = useState(
-  //     "https://hn.algolia.com/api/v1/search?query=productivity"
-  //   );
-  //   const [isError, setIsError] = useState(null);
-  //   const [isLoading, setIsLoading] = useState(false);
 
   const fetchDataReducer = (state, action) => {
     switch (action.type) {
@@ -35,25 +27,17 @@ export default function useArticles(initialUrl, initialData) {
 
   useEffect(() => {
     const fetchResults = async () => {
-      //   setIsError(false);
-      //   setIsLoading(true);
       dispatch({ type: "INITIAL_FETCH" });
 
       try {
         const response = await fetch(url);
         const data = await response.json();
-        // setData(data);
         dispatch({ type: "SUCCESSFUL_FETCH", payload: data });
       } catch (error) {
-        // console.log(error);
-        // setIsError(true);
         dispatch({ type: "FAILED_FETCH" });
       }
-      //   setIsLoading(false);
     };
     fetchResults();
   }, [url]);
   return [state, setUrl];
-
-  // return [{ data, isLoading, isError }, setUrl];
 }
