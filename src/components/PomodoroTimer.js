feab2e28ba52aba3246d42266cfc328ae03d09d1
Rethabile Segoe <precious.segoe@gmail.com>
2020-07-30 18:15:01 +0200

    remove input fields + their handlers

--- src/components/PomodoroTimer.js
+++ src/components/PomodoroTimer.js
@@ -28,20 +28,10 @@ export default function PomodoroTimer() {
     }
   }, [breakTime, timer, mode, session]);
 
-  const handleMainTimer = (event) => {
-    console.log("handle main time:", event.target.value * 60);
-    console.log("timer", timer);
-    setSession(event.target.value);
-    setTimer(event.target.value * 60); // Set main timer to this
-  };
   function handlePause() {
     setActive(!active);
   }
 
-  const handleBreak = (event) => {
-    console.log("break time:", event.target.value * 60);
-    setBreakTime(event.target.value * 60); // Set main timer to this
-  };
   // format a timer time in millisecods to a mm:ss format
   const formatTime = (time) => {
     let format = (val) => `0${Math.floor(val)}`.slice(-2);
@@ -88,15 +78,12 @@ export default function PomodoroTimer() {
     <div>
       <div className="maintimer">
         <h1> Timer: {formatTime(timer)}</h1>
-        <input type="number" defaultValue="25" onChange={handleMainTimer} />
-
         <button onClick={handleIncrease}>Increase</button>
         <button onClick={handleDecrease}>Decrease</button>
         <TimerControl pause={handlePause} reset={resetTimers} active={active} />
       </div>
       <div>
         <h1>Break: {formatTime(breakTime)}</h1>
-        {/* <input type="number" defaultValue="5" onChange={handleBreak} /> */}
         <button onClick={handleBreakIncrease}>Increase</button>
         <button onClick={handleBreakDecrease}>Decrease</button>
       </div>
